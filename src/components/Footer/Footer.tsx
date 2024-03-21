6aaf06b766b258afc84af43f08645a7bd51588f9
midu369 <chongsuiantian@gmail.com>
2024-03-21 18:14:42 +0900

    修正

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export const Footer: React.FC = () =>{
     return(
         <div className ="w-full h-20 bg-gray-400 text-center">
-            <Link href='https://www.link-u.co.jp'>@Link-U Inc.</Link>
+            <Link href=''></Link>
         </div>
     )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ export const Header: React.FC = () =>{
     return(
         
         <div className ="w-full h-40 bg-gray-400 text-center">
-            <img src = 'https://storage.googleapis.com/studio-design-asset-files/projects/7kadlQE8a3/s-510x142_webp_bc62b233-26b5-469d-bf21-9e0cc0e32862.webp' />
+            <img src = '' />
         </div>
     )
 }
