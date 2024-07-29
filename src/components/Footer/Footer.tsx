0acf83bcfe9ffacb8beab599f70dcb1d1ea6f203
Midu369 <156885131+Midu369@users.noreply.github.com>
2024-07-29 19:09:03 +0900

    一部UI変更

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,12 @@
 
 import React from "react";
-import Link from "next/link";
+//import Link from "next/link";
 
 
 export const Footer: React.FC = () =>{
     return(
         <div className ="w-full h-20 bg-gray-400 text-center">
-            <Link href='https://www.link-u.co.jp'>@Link-U Inc.</Link>
+            <h1>todoList</h1>
         </div>
     )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,9 @@ import React from "react";
 export const Header: React.FC = () =>{
     return(
         
-        <div className ="w-full h-40 bg-gray-400 text-center">
-            <img src = 'https://storage.googleapis.com/studio-design-asset-files/projects/7kadlQE8a3/s-510x142_webp_bc62b233-26b5-469d-bf21-9e0cc0e32862.webp' />
+        <div className ="w-full h-40 bg-gray-400 text-center flex">
+                <h1 className="text-[100px]">Mytask</h1>
+                {/* <img className="ml-20" src = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5B_1No4Binvb0ir9E90A0Zuxf4cw_i797R6LeF2rMR7CMxLDXtLuILSl5hzPS8uF1d1ep0X5wvexEp_w9ZSTM4BrxUDstyMMUgUG-6koVUu5lTd7QYAvO651JJ991dGZuTbuzxFZq2jcq/s400/document_list.png' /> */}
         </div>
     )
 }
